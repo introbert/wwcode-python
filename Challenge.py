c66f18a77059a8cf4ca8576e63b77cad5ef2a991
introbert <40712339+introbert@users.noreply.github.com>
2018-06-30 16:54:20 +0800

    Add files via upload

---
+++ Challenge.py
@@ -0,0 +1,7 @@
+totalbill = int(input("How much is your total bill? "))
+
+payment = int(input("How much is your payment? "))
+
+change =  payment - totalbill
+
+print("Hi! your change is {}" .format(change))
